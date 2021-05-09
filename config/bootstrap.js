d68d6e7682408fdd65ba1376a6dcffd8069324b7
Marcin Marciszewski <szantilas@gmail.com>
2021-05-09 19:48:33 +0100

    Moment imported.

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -75,5 +75,7 @@ module.exports.bootstrap = async function() {
   .tolerate((err)=>{
     sails.log.warn('For some reason, could not write bootstrap version .json file.  This could be a result of a problem with your configured paths, or, if you are in production, a limitation of your hosting provider related to `pwd`.  As a workaround, try updating app.js to explicitly pass in `appPath: __dirname` instead of relying on `chdir`.  Current sails.config.appPath: `'+sails.config.appPath+'`.  Full error details: '+err.stack+'\n\n(Proceeding anyway this time...)');
   });
+  
 
+  const moment = require('moment');
 };
