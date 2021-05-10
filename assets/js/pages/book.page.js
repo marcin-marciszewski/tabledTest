52de2a0020bc6df37c1d0d521bb350efb77a106e
Marcin Marciszewski <szantilas@gmail.com>
2021-05-10 07:42:21 +0100

    Single books styling.

--- assets/js/pages/book.page.js
+++ assets/js/pages/book.page.js
@@ -1,14 +1,20 @@
 parasails.registerPage("book", {
-    data: {
-      book: undefined,
-    },
-    
-    mounted: function(){
-    },
-    beforeMount: function () {
-      // Attach any initial data from the server.
-      _.extend(this, window.SAILS_LOCALS);
-    },
+  data: {
+    book: undefined,
+    published: '',
+    commentCreated: '',
+  },
   
-    methods: {},
-  });
+  mounted: function(){
+    this.published = moment(window.SAILS_LOCALS.book[0].publicationDate).format('DD/MM/YYYY');
+    this.commentCreated = moment(window.SAILS_LOCALS.book[0].creationTime).format('h:mm:ss a, DD/MM/YYYY ');;
+  },
+  beforeMount: function () {
+    // Attach any initial data from the server.
+    _.extend(this, window.SAILS_LOCALS);
+  },
+
+  methods: { 
+
+  },
+});
