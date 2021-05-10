dadddf8d54cc4a356b350ff0f7caa26424d21325
Marcin Marciszewski <szantilas@gmail.com>
2021-05-10 07:34:36 +0100

    Single article styling.

--- assets/js/pages/article.page.js
+++ assets/js/pages/article.page.js
@@ -1,11 +1,13 @@
 parasails.registerPage("article", {
     data: {
       article: undefined,
-      published: ''
+      published: '',
+      commentCreated: '',
     },
     
     mounted: function(){
       this.published = moment(window.SAILS_LOCALS.article[0].publicationDate).format('DD/MM/YYYY');
+      this.commentCreated = moment(window.SAILS_LOCALS.article[0].creationTime).format('h:mm:ss a, DD/MM/YYYY ');;
     },
     beforeMount: function () {
       // Attach any initial data from the server.
